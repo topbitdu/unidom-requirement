485aae228798b7a02b44593d9a3f1952d53aa798
Topbit Du <topbit.du@gmail.com>
2016-12-04 11:16:02 +0800

    1, Requirement model & migration.

---
+++ app/models/unidom/requirement/requirement.rb
@@ -0,0 +1,25 @@
+# Requirement 是需求，表示参与者的某种需要，并作用于某个设施。
+
+class Unidom::Requirement::Requirement < Unidom::Requirement::ApplicationRecord
+
+  self.table_name = 'unidom_requirements'
+
+  include Unidom::Common::Concerns::ModelExtension
+
+  validates :estimated_budget, presence: true, numericality: { less_than: 1_000_000_000, greater_than: 0 }
+  validates :quantity,         presence: true, numericality: { less_than: 1_000_000_000, greater_than: 0 }
+
+  belongs_to :superior_requirement, class_name:  'Unidom::Requirement::Requirement'
+  belongs_to :requirer,             polymorphic: true
+  belongs_to :required,             polymorphic: true
+  belongs_to :facility,             polymorphic: true
+  belongs_to :reason,               polymorphic: true
+
+  has_many :inferior_requirements, class_name: 'Unidom::Requirement::Requirement', foreign_key: :superior_requirement_id
+
+  scope :superior_requirement_is, ->(superior_requirement) { where superior_requirement_id: to_id(superior_requirement) }
+  scope :required_by,             ->(requirer)             { where requirer:                requirer                    }
+  scope :facility_is,             ->(facility)             { where facility:                facility                    }
+  scope :caused_by,               ->(reason)               { where reason:                  reason                      }
+
+end
---
+++ db/migrate/20021101000000_create_unidom_requirements.rb
@@ -0,0 +1,44 @@
+class CreateUnidomRequirements < ActiveRecord::Migration
+
+  def change
+
+    create_table :unidom_requirements, id: :uuid do |t|
+
+      t.references :superior_requirement, type: :uuid, null: true,
+        polymorphic: { null: true,  default: nil, limit: 200 }
+      t.references :requirer,             type: :uuid, null: false,
+        polymorphic: { null: false, default: '',  limit: 200 }
+      t.references :required,             type: :uuid, null: true,
+        polymorphic: { null: true,  default: nil, limit: 200 }
+      t.references :facility,             type: :uuid, null: false,
+        polymorphic: { null: false, default: '',  limit: 200 }
+      t.references :reason,               type: :uuid, null: true,
+        polymorphic: { null: true,  default: nil, limit: 200 }
+
+      t.decimal :estimated_budget, null: false, default: 0.0, precision: 12, scale: 2
+      t.decimal :quantity,         null: false, default: 0.0, precision: 12, scale: 2
+
+      t.date :required_on, null: false, default: nil
+
+      t.text :description
+      t.text :instruction
+
+      t.column   :state, 'char(1)', null: false, default: 'C'
+      t.datetime :opened_at,        null: false, default: Time.utc(1970)
+      t.datetime :closed_at,        null: false, default: Time.utc(3000)
+      t.boolean  :defunct,          null: false, default: false
+      t.jsonb    :notation,         null: false, default: {}
+
+      t.timestamps null: false
+
+    end
+
+    add_index :unidom_requirements, :superior_requirement_id
+    add_index :unidom_requirements, :requirer_id
+    add_index :unidom_requirements, :required_id
+    add_index :unidom_requirements, :facility_id
+    add_index :unidom_requirements, :reason_id
+
+  end
+
+end
